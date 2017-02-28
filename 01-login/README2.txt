45b574ecdd1516308c98c1f3334eb9233929c05b
alex <alopsa21@hotmail.com>
2017-02-28 09:44:40 +0000

    Ejemplo simple de login con Auth0. De la web the Auth0

---
+++ 01-login/README2.txt
@@ -0,0 +1,1 @@
+Este ejemplo es de Auth0. Solo para probar login con Angular2+Auth0. No serverside
---
+++ 01-login/app/auth.config.ts
@@ -0,0 +1,9 @@
+interface AuthConfiguration {
+    clientID: string,
+    domain: string
+}
+
+export const myConfig: AuthConfiguration = {
+    clientID: 'DlQAjipA7K74lqArREq2CPX9qEH5UY9l',
+    domain: 'alopsa21.eu.auth0.com'
+};
